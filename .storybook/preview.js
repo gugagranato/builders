fe71a97a587d176c49759f60e001b596b76f7ac2
“Gustavo <gustavo.granato@hotmail.com>
2022-09-16 00:00:35 -0300

    feat: add theme provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,13 @@
-import { RouterContext } from 'next/dist/shared/lib/router-context'
-import GlobalStyles from '../src/styles/global'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
-export const parameters = {
-  nextRouter: {
-    Provider: RouterContext.Provider
-  }
-}
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
